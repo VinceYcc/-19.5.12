3fba7724b2d8ce9756269c8a0330de8f3f101f10
VinceYu <1034350193@qq.com>
2019-06-12 15:44:44 +0800

    登录

--- A-hospital/static/js/common.js
+++
@@ -1,6 +0,0 @@
-function createXhr(){
-    if(window.XMLHttpRequest){
-        return new XMLHttpRequest();
-        }else{
-            return new ActiveXObject("Microsoft.XMLHTTP");
-            }}
